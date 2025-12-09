924026c6062b66db757862bc32558e6262fb7f74
Aryan Vishwakarma <official.yanish.mail@gmail.com>
2025-12-09 23:13:00 +0530

    skills and personal info

--- client/src/data/personalInfo.ts
+++ client/src/data/personalInfo.ts
@@ -1,6 +1,6 @@
 export const personalInfo = {
   name: "Aryan Vishwakarma",
-  title: "Full Stack Developer",
+  title: "Developer",
   description: "I build exceptional digital experiences that are fast, accessible, visually appealing, and responsive.",
   email: "aryanvishwakarma275@gmail.com",
   location: "Gorakhpur, Uttar Pradesh, India",
